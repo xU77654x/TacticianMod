71d979c477d17af58e2647e958f5ce2c50898517
Spencer Child <spencerchild947@protonmail.com>
2025-04-01 12:07:53 -0700

    first

--- src/main/java/tactician/cards/Common/OutdoorFighter.java
+++ src/main/java/tactician/cards/Common/OutdoorFighter.java
@@ -2,14 +2,10 @@ package tactician.cards.Common;
 
 import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.mod.stslib.patches.FlavorText;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import tactician.actions.OutdoorFollowUpAction;
 import tactician.cards.BaseCard;
 import tactician.character.MyCharacter;
@@ -35,31 +31,8 @@ public class OutdoorFighter extends BaseCard {
     }
 
     @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DrawCardAction(customVar("magicDraw"), (new OutdoorFollowUpAction(customVar("magicDraw"), this.block, this.magicNumber))));
-        // TODO: Make OutdoorGainAction not be hard-coded, such that gained Block can scale with Dexterity.
-    }
-
-    /*
-    public void gainBlock(int blockGain) {
-        AbstractPlayer p = AbstractDungeon.player;
-        this.baseBlock = this.block = blockGain;
-        applyPowers();
-        AbstractDungeon.actionManager.addToTop(new GainBlockAction(p, p, this.block));
-        this.baseBlock = 0;
-    }
-
-    public void gainVigor(int vigorGain) {
-        AbstractPlayer p = AbstractDungeon.player;
-        int realMagic = baseMagicNumber;
-        baseMagicNumber += this.magicNumber = vigorGain;
-        applyPowers();
-        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new VigorPower(p, this.magicNumber), this.magicNumber));
-        baseMagicNumber = realMagic;
-    } */
+    public void use(AbstractPlayer p, AbstractMonster m) { addToBot(new DrawCardAction(customVar("magicDraw"), (new OutdoorFollowUpAction(customVar("magicDraw"), this.block, this.magicNumber)))); }
 
     @Override
-    public AbstractCard makeCopy() {
-        return new OutdoorFighter();
-    }
+    public AbstractCard makeCopy() { return new OutdoorFighter(); }
 }
