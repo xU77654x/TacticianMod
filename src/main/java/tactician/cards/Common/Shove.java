37089e1b9a7c6ae4ecb651bdf71f59a09c0d4820
Spencer Child <spencerchild947@protonmail.com>
2025-04-02 14:42:15 -0700

    first

--- src/main/java/tactician/cards/Common/Shove.java
+++ src/main/java/tactician/cards/Common/Shove.java
@@ -7,7 +7,6 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawPower;
 import tactician.cards.BaseCard;
 import tactician.character.MyCharacter;
 import tactician.powers.ShovePower;
