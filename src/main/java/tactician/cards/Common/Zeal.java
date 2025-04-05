72bf79e963cc3411d8e1ca701b9c9341b2e7df03
Spencer Child <spencerchild947@protonmail.com>
2025-04-05 15:24:48 -0700

    first

--- src/main/java/tactician/cards/Common/Zeal.java
+++ src/main/java/tactician/cards/Common/Zeal.java
@@ -3,15 +3,16 @@ package tactician.cards.Common;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.LoseStrengthPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.*;
 import tactician.cards.BaseCard;
 import tactician.character.MyCharacter;
 import tactician.util.CardStats;
 
 public class Zeal extends BaseCard {
     public static final String ID = makeID(Zeal.class.getSimpleName());
+    AbstractPlayer p;
     private static final CardStats info = new CardStats(
             MyCharacter.Meta.CARD_COLOR,
             CardType.SKILL,
@@ -22,16 +23,17 @@ public class Zeal extends BaseCard {
 
     public Zeal() {
         super(ID, info);
-        setMagic(2, 1);
+        setMagic(1, 1);
+        this.p = AbstractDungeon.player;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
-        addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, this.magicNumber), this.magicNumber));
-
-        // TODO: If you have negative Strength, Focus, or Dexterity, cure it.
-        // TODO: ZealPower; your cards are weapon-strong for X turns.
+        addToBot(new ApplyPowerAction(p, p, new FeelNoPainPower(p, magicNumber), magicNumber));
+        if (this.p.hasPower(StrengthPower.POWER_ID) && (this.p.getPower(StrengthPower.POWER_ID)).amount < 0) { this.p.powers.remove(this.p.getPower(StrengthPower.POWER_ID)); }
+        if (this.p.hasPower(FocusPower.POWER_ID) && (this.p.getPower(FocusPower.POWER_ID)).amount < 0) { this.p.powers.remove(this.p.getPower(FocusPower.POWER_ID)); }
+        if (this.p.hasPower(DexterityPower.POWER_ID) && (this.p.getPower(DexterityPower.POWER_ID)).amount < 0) { this.p.powers.remove(this.p.getPower(DexterityPower.POWER_ID)); }
+        // TODO: ZealPower
     }
 
     @Override
