c8554dd4f7c56f39739f8cfce23ac6a20c49db0c
Spencer Child <spencerchild947@protonmail.com>
2025-07-10 00:58:39 -0700

    Icons for relics and powers, including weapon types. Many various bug fixes.

--- src/main/java/tactician/actions/RandomExhumeAction.java
+++ src/main/java/tactician/actions/RandomExhumeAction.java
@@ -4,31 +4,26 @@ import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RandomExhumeAction extends AbstractGameAction {
-    private AbstractPlayer p;
     private final int count;
-    private final boolean isupgraded;
+    private final boolean upgraded;
 
     public RandomExhumeAction(int count, boolean upgraded) {
         this.count = count;
-        this.p = AbstractDungeon.player;
-        setValues(this.p, AbstractDungeon.player, this.amount);
         this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
         this.duration = Settings.ACTION_DUR_FAST;
-        this.isupgraded = upgraded;
+        this.upgraded = upgraded;
     }
 
     private void exhumeRandom(AbstractCard c) {
-        if (this.isupgraded) { c.upgrade(); }
-        if (this.p.hand.size() >= BaseMod.MAX_HAND_SIZE) {
-            this.p.discardPile.addToTop(c);
-        } else { this.p.hand.addToHand(c); }
+        if (this.upgraded) { c.upgrade(); }
+        if (AbstractDungeon.player.hand.size() >= BaseMod.MAX_HAND_SIZE) { AbstractDungeon.player.discardPile.addToTop(c); }
+        else { AbstractDungeon.player.hand.addToHand(c); }
         c.unfadeOut();
         c.unhover();
         c.fadingOut = false;
@@ -37,7 +32,7 @@ public class RandomExhumeAction extends AbstractGameAction {
     public void update() {
         int cardsReturned = 0;
         if (this.duration == Settings.ACTION_DUR_FAST) {
-            if (this.p.exhaustPile.isEmpty()) {
+            if (AbstractDungeon.player.exhaustPile.isEmpty()) {
                 this.isDone = true;
                 return;
             }
@@ -61,7 +56,7 @@ public class RandomExhumeAction extends AbstractGameAction {
                         if (cardsReturned == this.count) { break; }
                     }
                 }
-            for (AbstractCard c : cardsToExhaust) { this.p.exhaustPile.removeCard(c); }
+            for (AbstractCard c : cardsToExhaust) { AbstractDungeon.player.exhaustPile.removeCard(c); }
             this.isDone = true;
             return;
         }
