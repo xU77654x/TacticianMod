5f1b84208fa3eb4c8ce768577d48038aa90ee5da
Spencer Child <spencerchild947@protonmail.com>
2025-09-08 11:17:26 -0700

    1.0.7 release

--- src/main/java/tactician/cards/common/Reposition.java
+++ src/main/java/tactician/cards/common/Reposition.java
@@ -2,7 +2,6 @@ package tactician.cards.common;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
